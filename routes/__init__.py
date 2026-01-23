a16b73ffabcb91609e2c4b76010d1728c4375a51
Ashwani Garg <ashwanigarg@outlook.in>
2026-01-23 17:45:10 +0530

    Add channel routes registration: include register_channels in __all__ for route management in __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,4 +1,5 @@
 from routes.api import register_api_routes
 from routes.web import register_web_routes
+from routes.channels import register_channels
 
-__all__ = ["register_api_routes", "register_web_routes"]
+__all__ = ["register_api_routes", "register_web_routes", "register_channels"]
